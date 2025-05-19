e9a998c49b147410b9a8a36729d1ea101e8c194a
lymbf <paw.czekaj@gmail.com>
2025-05-20 00:17:44 +0200

    pages:articles: supabase data select; getting imgurl; test-article implementation

--- app/@header/page.tsx
+++ app/@header/page.tsx
@@ -5,12 +5,15 @@ import {ThemeSwitcher} from "@/components/theme-switcher";
 
 export default function Default() {
     return <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-5xl flex justify-end items-center p-3 px-5 text-sm">
+        <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
 
             </div>
-            {!hasEnvVars ? <EnvVarWarning/> : <HeaderAuth/>}
-            <ThemeSwitcher/>
+            <div className={'flex flex-row'}>
+                {!hasEnvVars ? <EnvVarWarning/> : <HeaderAuth/>}
+                <ThemeSwitcher/>
+            </div>
+
         </div>
     </nav>
 }
--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,9 +1,50 @@
 import {cn} from "@/lib/utils";
+import Image from "next/image";
+import {createServerClient} from "@supabase/ssr";
+import {createClient} from "@/utils/supabase/server";
 
-export default function Page(){
+const getServerSideProps = () => {
+    return {
+        props: {}
+    }
+}
+
+export default async function Page() {
+
+    const supabase = await createClient()
+
+    const {
+        data,
+        error
+    } = await supabase.from("articles")
+        .select(`title,text, category (id,name), img_file, subcategory (id,name)`)
+        .overrideTypes<Array<{
+            title: string,
+            text: string,
+            img_file:string,
+            category: { id: string, name: string },
+            subcategory: { id: string, name: string }
+        }>, { merge: false }>()
+
+    console.log('data: ', data)
+    console.log('error: ', error)
+
     return (
         <main className="flex-1 flex flex-col gap-6 px-4">
-
+            <div className={'flex flex-col gap-6'}>
+                {data?.map(async (a, i) => {
+                    const imgUrl = await supabase.storage.from('images').getPublicUrl(a.img_file).data.publicUrl
+                    return <div key={i} className={'flex flex-col'}>
+                        <p className={'mb-[10px] text-[12px] font-medium capitalize'}>{a.category.name} - <span
+                            className={'text-[12px] font-normal'}>{a.subcategory.name}</span></p>
+                        <h1 className={'text-3xl mb-[20px] font-medium'}>{a.title}</h1>
+                        <Image src={imgUrl} alt={a.img_file} width = {500} height = {500}/>
+                        <p className={''}>
+                            {a.text}
+                        </p>
+                    </div>
+                })}
+            </div>
         </main>
     )
 }
